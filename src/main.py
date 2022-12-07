61d7929e4a269e9d04ed5199ea1b3ab7a4ae8e0b
chena11356 <achen1800@gmail.com>
2022-12-06 23:07:07 -0500

    update json pre-processing, add csv

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import requests
 from pprint import pprint
 from datetime import datetime as dt
 import time
+import pandas
 
 boards = [
     '3','a','aco','adv','an','b','bant',
@@ -18,65 +19,25 @@ boards = [
     'vst','vt','w','wg','wsg','wsr','x',
     'xs','y']
 
-timestr = time.strftime("%Y%m%d-%H") # TODO: https://betterprogramming.pub/https-medium-com-ratik96-scheduling-jobs-with-crontab-on-macos-add5a8b26c30
+timestr = time.strftime("%Y%m%d-%H") 
 
-for board in boards:
+# for board in boards:
 
-    r = requests.get('https://a.4cdn.org/' + board + '/catalog.json')
-    r = r.json()
+board = 'b'
 
-    r = requests.get('https://a.4cdn.org/' + board + '/catalog.json')
-    r = json.loads(r.text)
+r = requests.get('https://a.4cdn.org/' + board + '/catalog.json')
+r = json.loads(r.text)
 
-    filename = '../data/' + board + '/' + board + 'Data' + timestr + '.json'
+threads = []
+for page in r: 
+    for thread in page['threads']:
+        threads.append(thread)
 
-    with open(filename, 'w') as json_file:
-        json.dump(r, json_file) #code for writing to a json_file
+filename_json = 'data/' + board + '/' + board + 'Data' + timestr + '.json'
+filename_csv = 'data/' + board + '/' + board + 'Data' + timestr + '.csv'
 
+with open(filename_json, 'w') as json_file:
+    json.dump(threads, json_file) # write to json file
 
-    # print(r[0]["threads"])  # nested info
-
-    # print(r[0]["threads"][0]["com"])  # specific text on one thread
-
-    # pprint(r)
-
-    #words to use - trump
-
-    #4CHAN CODE 
-
-    def gen_chan():
-        for idx, page in enumerate(r):
-            for thread in r[idx]['threads']:
-                yield thread
-
-
-    def get_threads(key: str, default='NaN'):
-        return threads.get(key, default)
-
-
-    for threads in gen_chan():
-        no = get_threads('no')
-        now = get_threads('now')
-        time = get_threads('time')
-        my_time = dt.today()
-        # com = TextMaster.strip_html(get_threads('com'))
-        name = get_threads('name')
-        trip = get_threads('trip')
-        ids = get_threads('id')
-        capcode = get_threads('capcode')
-        filename = get_threads('filename') + get_threads('ext')
-        resto = get_threads('resto')
-        semantic_url = get_threads('semantic_url')
-        replies = get_threads('replies')
-        images = get_threads('images')
-        # url = TextMaster.extract_url(get_threads('com'))
-        # sent = TextMaster.textblob_sentiment(get_threads('com'))
-        if 'last_replies' in threads:
-            for comment in threads['last_replies']:
-                com_com = comment.get('com', 'NaN')
-                resto_com = comment.get('resto', 'NaN')
-                now_com = comment.get('now', 'NaN')
-                time_com = comment.get('time', 'NaN')
-                fname_com = comment.get('filename', 'NaN')
-                url_com = comment.get('com')
-                sent_com = comment.get('com')
+csv_data = pandas.json_normalize(threads)
+csv_data.to_csv(filename_csv) # write to csv file
